a28be3c2338458ccc389498630f50ae026a6c654
MindenNZN <minde.ziur@gmail.com>
2020-10-27 17:00:23 +0200

    Cards deck

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php 'veikia';
+<?php
+$cards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2];
+$kinds = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+
+$card_deck = [];
+
+foreach ($cards as $keys => $card) {
+    foreach ($kinds as $key => $kind) {
+        $card_deck[] = [
+            'kind' => $kind,
+            'card' => $card
+        ];
+    }
+}
+var_dump($card_deck);
+?>
