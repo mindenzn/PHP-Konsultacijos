2f04a66ff947bb8a6c1071b995d7123916fe1728
MindenNZN <minde.ziur@gmail.com>
2020-10-28 14:09:00 +0200

    kortos

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-$array = [9, 1, 2, 1, 4, 10, 5, 6, 6, 10, 6, 7];
-
-$result = array_unique($array);
-
-$array_new_keys = array_values($result);
-
-var_dump($array_new_keys);
+$cards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+$kinds = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+$card_deck = [];
+foreach ($cards as $keys => $card) {
+    foreach ($kinds as $key => $kind) {
+        $card_deck[] = [
+            'kind' => $kind,
+            'card' => $card
+        ];
+    }
+}
+var_dump($card_deck);
 ?>
