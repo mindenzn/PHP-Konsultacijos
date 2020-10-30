4427a837627ff8f1238b3bfb220eafe6a86add52
MindenNZN <minde.ziur@gmail.com>
2020-10-30 16:19:13 +0200

    Filmu Listas

--- index.php
+++ index.php
@@ -1,91 +1,43 @@
 <?php
-/**
- * @return array
- * Sukuriamas ,,card_deck'' masyvas is ,,card'' ir ,,type''
- */
-function deck()
+$movies = [
+    [
+        'movie_img' => 'https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg',
+        'movie_title' => 'Avengers Saga',
+        'movie_director' => 'Anthony and Joe Russo',
+        'movie_genres' => ['Action', 'Drama', 'Fantasy'],
+        'main_actors' => ['Robert Downey Jr', ' Chris Evans', ' Scarlett Johansson'],
+        'movie_date' => '2012'
+    ],
+    [
+        'movie_img' => 'https://cdn.hmv.com/r/w-1280/hmv/files/35/358d9ebd-3be4-4ef3-a412-9c0cbfc626e3.jpg',
+        'movie_title' => 'Lord of the Rings Saga',
+        'movie_director' => 'Peter Jackson',
+        'movie_genres' => ['Action', 'Drama', 'Fantasy'],
+        'main_actors' => ['Orlando Bloom', 'Liv Tyler', 'John Rhys-Davies'],
+        'movie_date' => '2001'
+    ],
+    [
+        'movie_img' => 'https://play-lh.googleusercontent.com/xo2HfLoAszntYndTjrUhZXqa7xCmeSkSXxcsPPeQx3-cRrzYSGmbjSwKO2F7o-RWuJhy',
+        'movie_title' => 'Harry Potter Saga',
+        'movie_director' => 'David Heyman',
+        'movie_genres' => ['Drama', 'Fantasy', 'Family'],
+        'main_actors' => ['Daniel Radcliffe', 'Rupert Grint', 'Emma Watson'],
+        'movie_date' => '2003'
+    ]
+];
+function list_movie_by_year($array, $year)
 {
-    $number = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2];
-    $type = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
-    $card_deck = [];
-    foreach ($number as $keys => $card) {
-        foreach ($type as $key => $kind) {
-            $card_deck[] = [
-                'type' => $kind,
-                'number' => $card
-            ];
+    $movies_from = [];
+    foreach ($array as $movie) {
+        if ($movie['movie_date'] > $year) {
+            $movies_from[] = $movie;
         }
     }
-    return $card_deck;
+    return $movies_from;
 }
 
-$card_deck = deck();
-/**
- * @param $deck
- * @return array
- * paduodamas naujai sugeneruotas visu kortu masyvas ,,card_deck'' kad random atvaizuotu 5 is ju
- */
-function draw_card(array $deck): array
-{
-    $five_cards = [];
-    for ($i = 1; $i <= 5; $i++) {
-        $card_name = rand(0, count($deck) - 1);
-        $five_cards[] = $deck[$card_name];
-        array_splice($deck, $card_name, 1);
+$movies_from = list_movie_by_year($movies, 2005);
 
-
-    }
-    return $five_cards;
-}
-
-$cards_on_table = draw_card($card_deck);
-/**
- *paduodam sugeruotas 5 kortas i funkcija kad patikrintu ar visos vienodos rusies
- */
-function check_type($cards_on_table)
-{
-    $same_card = 0;
-    for ($a = 0; $a < count($cards_on_table); $a++) {
-        if ($cards_on_table[$a]['type'] === $cards_on_table[0]['type']) {
-            $same_card += 1;
-        }
-    }
-    $text = '';
-    if ($same_card === count($cards_on_table)) {
-        $text = 'Yes';
-    } else $text = 'No';
-
-    return $text;
-}
-
-$answer_same_type = check_type($cards_on_table);
-
-function check_card($cards_on_table)
-{
-    $same = [];
-    for ($i = 0; $i < 5; $i++) {
-        if (isset($same[$cards_on_table[$i]['number']])) {
-            $same[$cards_on_table[$i]['number']]++;
-        } else {
-            $same[$cards_on_table[$i]['number']] = 1;
-        }
-    }
-    return $same;
-}
-
-
-$same = check_card($cards_on_table);
-
-
-function card_pair($array){
-    foreach ($array as $key => $value) {
-        if($value >= 2) {
-            return 'YES';
-        } else return 'NO';
-    }
-}
-
-$pair_answer = 'Do i have pair? :' . card_pair($same);
 ?>
 <!doctype html>
 <html lang="en">
@@ -99,14 +51,19 @@ $pair_answer = 'Do i have pair? :' . card_pair($same);
 </head>
 <body>
 <section>
-    <?php foreach ($cards_on_table as $on_table): ?>
-        <div class="card <?php print $on_table['type']; ?>"><?php print $on_table['type']; ?>
-            <h2><?php print $on_table['number']; ?></h2>
-        </div>
+    <?php foreach ($movies as $movie): ?>
+        <article>
+            <h1><?php print $movie['movie_title'] ?></h1>
+            <h3>Written by: <?php print $movie['movie_director'] ?></h3>
+            <img src="<?php print $movie['movie_img'] ?>">
+            <h3>Movie genres: <?php print implode(', ', $movie['movie_genres']) ?></h3>
+            <h3>Main Actors:</h3>
+            <?php foreach ($movie['main_actors'] as $actor): ?>
+                <li><?php print $actor ?></li>
+            <?php endforeach; ?>
+            <h3>Year: <?php print $movie['movie_date'] ?></h3>
+        </article>
     <?php endforeach; ?>
 </section>
-<h3>Do i have flush? : <?php print $answer_same_type; ?></h3>
-<h3><?php print $pair_answer ?></h3>
-<h2>Done</h2>
 </body>
 </html>
