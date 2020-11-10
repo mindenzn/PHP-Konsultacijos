fed0104b752d8fd6989a7acfbe8273653f80b413
MindenNZN <minde.ziur@gmail.com>
2020-11-10 14:45:20 +0200

    Savaites dienos

--- index.php
+++ index.php
@@ -1,24 +1,31 @@
 <?php
-if (!empty($_POST['submit'])) {
-    $text = date('l');
-    {
-        if ($text == 'Monday') {
-            $text = 'Pirmadienis';
-        } elseif ($text == 'Tuesday') {
-            $text = 'Antradienis';
-        } elseif ($text == 'Wednesday') {
-            $text = 'Treciadienis';
-        } elseif ($text == 'Thursday ') {
-            $text = 'Ketvirtadienis';
-        } elseif ($text == 'Friday') {
-            $text = 'Penktadienis';
-        } elseif ($text == 'Saturday ') {
-            $text = 'Sestadienis';
-        } elseif ($text == 'Sunday') {
-            $text = 'Sekmadienis';
-        }
-    }
-} else $text = 'Spausk mygtuka ir suzinok data'
+$string = 'kuKUUUUkuUUku';
+$new_string = 'kuKUUUUkuUUkuttewww';
+/**
+ * @param $string
+ * @return string|string[]
+ */
+function remove_vowels($string)
+{
+    $vowels = array("a", "e", "i", "o", "u", " ");
+    $string = str_ireplace($vowels, "", $string);
+    return $string;
+
+}
+
+/**
+ * @param $new_string
+ */
+function remove_vowels_reference(&$new_string)
+{
+    $vowels = array("a", "e", "i", "o", "u", " ");
+    $new_string = str_ireplace($vowels, "", $new_string);
+}
+
+var_dump(remove_vowels($string));
+
+remove_vowels_reference($new_string);
+var_dump($new_string);
 
 
 ?>
@@ -29,15 +36,9 @@ if (!empty($_POST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-<style>
-
-</style>
 <body>
-<form method="POST">
-    <input type="submit" name="submit" value="Atsapausdinti data">
-</form>
-<h1><?php print $text ?></h1>
 </body>
 </html>
